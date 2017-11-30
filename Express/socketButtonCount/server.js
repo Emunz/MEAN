99b155879bfde7d3e6e6db01a6bde808e6a445b8
Evan Munz <emunz14@gmail.com>
2017-11-30 16:41:41 -0600

    added the counter button game with socket

---
+++ Express/socketButtonCount/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+
+var path = require("path");
+var session = require('express-session');
+
+var app = express();
+var bodyParser = require('body-parser');
+app.use(session({secret: 'codingdojorocks'}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+var count = 0
+
+app.get('/', function (req, res) {
+    res.render("index", {count: count});
+})
+
+var server = app.listen(8000, function () {
+    console.log("listening on port 8000");
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+    console.log("Client/socket is connected!");
+    console.log("Client/socket id is: ", socket.id);
+    // all the server socket code goes in here
+    
+    socket.on( "add_clicked", function (data){
+        count++
+        io.emit( 'server_response', {response: count});
+    })
+
+    socket.on( "reset_clicked", function (data){
+        count = 0
+        io.emit( 'server_response', {response: count});
+    })
+  })
